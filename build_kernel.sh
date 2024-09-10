bbab980d825f63140a6617633144a1fae7ea01ec
= mlm-games <= gerrit.r6a6a@passinbox.com>
2024-09-10 08:40:40 +0530

    add lost changes

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
-
-root_dir=$(pwd)
-cd ${root_dir} && cd ..
-kernel_dir=`pwd` && cd ${root_dir}
-echo "kernel_dir=$kernel_dir"
 rm -rf out
 make clean && make mrproper
 mkdir out
-# export TARGET_ARCH_VARIANT=armv7-a-neon
-export CROSS_COMPILE="$kernel_dir/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-export ARCH=arm64
 
-make ARCH=arm64 O=out aud8516p1_64_basic_debug_defconfig
+export CROSS_COMPILE="$(pwd)/../aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export ARCH=arm64
+export O=out
+
+make aud8516p1_64_basic_debug_defconfig
+
 status=${PIPESTATUS[0]}
 if [ "$status" != "0" ]; then 
      echo "ERROR:  make defconfig  error:"
@@ -20,7 +17,8 @@ if [ "$status" != "0" ]; then
 else
      echo "#### make defconfig successfully ####"
 fi
-make ARCH=arm64 -j$(nproc) O=out
+make -j$(nproc)
+
 re=${PIPESTATUS[0]}
 if [ "$re" != "0" ];then 
      echo "ERROR:  build kernel error:"
